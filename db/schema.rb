d4fce900e9aad982a03cdbc0462385cbb24ba337
gr347wh173n0r7h <jordan.a.petersen@gmail.com>
2016-04-28 20:07:33 -0700

    Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313102914) do
+ActiveRecord::Schema.define(version: 20160429000103) do
 
   create_table "advisors", force: :cascade do |t|
     t.string   "name"
@@ -33,5 +33,15 @@ ActiveRecord::Schema.define(version: 20160313102914) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
